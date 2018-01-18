f31bd877818010c7c2e3c71a739fbfaad5a98266
Keegan McGrath <keegan.mcgrath@gmail.com>
2018-01-18 10:51:30 -0800

    Finish exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,30 +2,39 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c('crystal', 'quartz', 'emerald', 'diamond', 'coral', 'aquamarine', 'gold')
 
 # Create a vector `words.of.the.day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (single or double) (e.g., `"data" is the 
 # word of the day!`)
-
+words.of.the.day <- paste('\'', words, "\' is the word of the day!", sep="")
+print(words.of.the.day)
 
 # Create a vector `a.f.words` which are the elements in `words` that start with 
 # "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
-
+a.f.words <- words[substring(words, 1, 1) <= 'f']
+print(a.f.words)
 
 # Create a vector `g.m.words` which are the elements in `words` that start with 
 # "g" through "m"
-
+g.m.words <- words[substring(words, 1, 1) >= 'g' & substring(words, 1, 1) <= 'm']
+print(g.m.words)
 
 # Define a function `WordBin` that takes in three arguments: a vector of words, 
 # and two letters. The function should return a vector of words that go between 
 # those letters alphabetically.
+WordBin <- function(words, l1, l2) {
+  if (l1 < l2) {
+    return(WordBin(words, l2, l1))
+  }
+  return(words[substring(words, 1, 1) <= l1 & substring(words, 1, 1) >= l2])
+}
 
 
 # Use your `WordBin` function to determine which of your words start with "e" 
 # through "q"
-
+print(WordBin(words, 'q', 'e'))
